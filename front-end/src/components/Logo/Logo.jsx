1f0f279f74308d17fe03147dd52a103262018f14
Vectari <mateusz.majer996@gmail.com>
2024-04-26 23:46:45 +0200

    add Logo

---
+++ front-end/src/components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const StyledLogo = styled.h1`
+  text-transform: uppercase;
+`;
+
+export function Logo() {
+  return <StyledLogo>TopSklep®</StyledLogo>;
+}
