e1a4a0054a7bcbd010f3469b096a3685f456472f
Vectari <mateusz.majer996@gmail.com>
2024-04-27 12:31:15 +0200

    add FullWidthButton

---
+++ front-end/src/components/FullWidthButton/FullWidthButton.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+import { Theme } from "../../styles/Theme";
+
+const StyledButton = styled.button`
+  border: none;
+  background: none;
+  width: 100%;
+  padding: 1rem 0;
+  color: ${Theme.color.white};
+  text-transform: uppercase;
+  background-color: ${Theme.color["theme-main"]};
+  font-weight: 700;
+`;
+
+export function FullWidthButton({ children, onClick }) {
+  return <StyledButton onClick={onClick}>{children}</StyledButton>;
+}
