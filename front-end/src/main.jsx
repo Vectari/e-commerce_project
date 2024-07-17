3f73ee3aa781a85710ff12bdb5480e0f0477d725
Vectari <mateusz.majer996@gmail.com>
2024-07-17 21:25:03 +0200

    feat: integrate MainPage with backend

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -2,10 +2,15 @@ import "./styles/theme.css";
 import "./styles/globals.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
 import { Cart } from "./views/Cart/Cart";
 import { Favourites } from "./views/Favourites/Favourites";
 import { Layout } from "./components/Layout/Layout";
+import { MainPage } from "./views/MainPage/MainPage";
 
 const router = createBrowserRouter([
   {
@@ -19,6 +24,24 @@ const router = createBrowserRouter([
       {
         path: "/ulubione",
         element: <Favourites />,
+      },
+      {
+        path: "/:gender",
+        element: <MainPage />,
+        loader: ({ params }) => {
+          const PATH_TO_ENDPOINT_MAPPING = {
+            kobieta: "women",
+            mezczyzna: "men",
+            dziecko: "children",
+          };
+
+          const backEndPath = PATH_TO_ENDPOINT_MAPPING[params.gender];
+          if (backEndPath) {
+            return fetch(`http://localhost:3000/${backEndPath}`);
+          } else {
+            return redirect("/kobieta");
+          }
+        },
       },
     ],
   },
