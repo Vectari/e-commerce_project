44c1a23b2d99030e45112c39e24640ae37846b6b
Vectari <mateusz.majer996@gmail.com>
2024-08-22 23:24:18 +0200

    chore: delete Layout

--- front-end/src/views/ProductsList/ProductsList.jsx
+++ front-end/src/views/ProductsList/ProductsList.jsx
@@ -1,4 +1,3 @@
-import { Layout } from "../../components/Layout/Layout";
 import { FlexContainer } from "../../components/FlexContainer/FlexContainer";
 import { ExpandableMenu } from "../../components/ExpandableMenu/ExpandableMenu";
 import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs";
@@ -162,15 +161,13 @@ export function ProductsList() {
   ];
 
   return (
-    <Layout>
-      <FlexContainer>
-        <ExpandableMenu />
-        <div>
-          <Breadcrumbs />
-          <Products headerText="Swetry" products={products} />
-          <Pagination numberOfPages={5}/>
-        </div>
-      </FlexContainer>
-    </Layout>
+    <FlexContainer>
+      <ExpandableMenu />
+      <div>
+        <Breadcrumbs />
+        <Products headerText="Swetry" products={products} />
+        <Pagination numberOfPages={5} />
+      </div>
+    </FlexContainer>
   );
 }
